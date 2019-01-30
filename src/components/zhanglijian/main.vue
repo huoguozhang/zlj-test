4630115f59e86695e89212ee9d369679c9890ce5
zhanglijian <zhanglijianwork@163.com>
2019-01-30 10:29:15 +0800

    npm run script

---
+++ src/components/zhanglijian/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="zhanglijian-page-ct">
+    zhanglijian组件
+  </div>
+</template>
+<script>
+export default {
+  name: 'zhanglijian'
+}
+</script>
+<style lang="scss" scoped>
+.zhanglijian-page-ct {
+
+}
+</style>
